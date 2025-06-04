770aa15767f558e8ed69c9dd2b3c99e7ef055ed0
yahia shawky <yahyashaeky@gmail.com>
2025-06-04 10:14:49 +0300

    pagination in halls

--- src/pages/Halls.tsx
+++ src/pages/Halls.tsx
@@ -10,6 +10,14 @@ import { useTranslation } from "react-i18next";
 import LoadingSpinner from "@/components/ui/loader";
 import axiosInstance from "@/axiosConfig/instance";
 import { Link } from "react-router-dom";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 interface Hall {
   id: number;
@@ -23,27 +31,35 @@ const Halls = () => {
   const { t, i18n } = useTranslation();
   const [halls, setHalls] = useState<Hall[]>([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 3; 
+  const [totalCount, setTotalCount] = useState(0);
 
-  const gethHalls = async () => {
+  const getHalls = async (page: number, size: number) => {
     try {
       setLoading(true);
-
       const langParam = i18n.language === "en" ? "en" : "";
       const response = await axiosInstance.get(
-        `/Hall/GetAll?language=${langParam}`
+        `/Hall/GetAll?language=${langParam}&PageNumber=${page}&PageSize=${size}`
       );
-      setHalls(response.data.data || []);
+      setHalls(response.data.data.result || []);
+      setTotalCount(response.data.data.totalCount); 
     } catch (error) {
       console.error("Error fetching halls", error);
     } finally {
       setLoading(false);
     }
   };
+  console.log(halls)
+
   useEffect(() => {
-    gethHalls();
-  }, [i18n.language]);
-  
+    getHalls(currentPage, pageSize);
+  }, [currentPage, i18n.language]);
+
   if (loading) return <LoadingSpinner />;
+
+
+  const totalPages = Math.ceil(totalCount / pageSize);
 
   return (
     <section
@@ -94,6 +110,29 @@ const Halls = () => {
             <p className="text-gray-500">{t("noHallsAvailable")}</p>
           </div>
         )}
+
+        {/* Pagination */}
+        <Pagination className='mb-11 mt-11'>
+          {i18n.language === "ar" ? 
+            <PaginationNext onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} /> : 
+            <PaginationPrevious onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} /> }
+          <PaginationContent dir='ltr'>
+            {Array.from({ length: totalPages }, (_, index) => (
+              <PaginationItem key={index + 1}>
+                <PaginationLink
+                  isActive={currentPage === index + 1}
+                  onClick={() => setCurrentPage(index + 1)}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+          </PaginationContent>
+          {i18n.language === "ar" ?
+            <PaginationPrevious onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} />  : 
+            <PaginationNext onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} />
+          }
+        </Pagination>
       </div>
     </section>
   );
